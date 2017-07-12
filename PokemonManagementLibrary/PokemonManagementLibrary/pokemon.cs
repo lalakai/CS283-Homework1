e6e47dc5d384cb2f4174318973da5761a867cd22
lalakai <dincy0919@gmail.com>
2017-07-12 23:40:34 +0800

    新增寶可夢管理套件

---
+++ PokemonManagementLibrary/PokemonManagementLibrary/pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonManagementLibrary
+{
+    public class pokemon
+    {
+        public string name;
+        public string hp;
+        public string gender;
+        public string weight;
+        public string attribute;
+        public string height;
+        public string skill1;
+        public string skill2;
+    }
+}
---
+++ homework01-pokemon/pokemon01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonManagementLibrary;
+
+namespace pokemon01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            pokemon pokemon = new pokemon()
+            {
+                name = "妙蛙種子",
+                hp = "60",
+                gender = "男",
+                weight = "5.94kg",
+                attribute = "草/毒",
+                height = "0.67m",
+                skill1 = "藤鞭",
+                skill2 = "汙泥炸彈",
+            };
+            Console.WriteLine("名稱:{0}, 血量:{1}, 性別:{2}, 體重:{3}, 屬性:{4}, 身高:{5}," +
+                "技能1:{6}, 技能2:{7}"
+                , pokemon.name
+                , pokemon.hp
+                , pokemon.gender
+                , pokemon.weight
+                , pokemon.attribute
+                , pokemon.height
+                , pokemon.skill1
+                , pokemon.skill2
+                );
+        }
+    }
+}
