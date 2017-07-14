26e543bf767f6a31faa25eb9705c3ca77daaaaf9
lalakai <dincy0919@gmail.com>
2017-07-14 12:35:04 +0800

    作業1完成

--- PokemonManagementLibrary/PokemonManagementLibrary/pokemon.cs
+++ PokemonManagementLibrary/PokemonManagementLibrary/pokemon.cs
@@ -12,9 +12,17 @@ namespace PokemonManagementLibrary
         public string hp;
         public string gender;
         public string weight;
+        public string star;
+        public string candy;
+        public string powerup;
+        public string evolve;
         public string attribute;
         public string height;
         public string skill1;
+        public string skill1dmg;
         public string skill2;
+        public string skill2dmg;
+        public string location;
+        public string date;
     }
 }
--- homework01-pokemon/pokemon01/Program.cs
+++ homework01-pokemon/pokemon01/Program.cs
@@ -19,19 +19,36 @@ namespace pokemon01
                 weight = "5.94kg",
                 attribute = "草/毒",
                 height = "0.67m",
+                star = "271484",
+                candy = "27",
+                powerup = "3000星星+3糖果",
+                evolve = "25糖果",
                 skill1 = "藤鞭",
+                skill1dmg = "7",
                 skill2 = "汙泥炸彈",
+                skill2dmg = "80",
+                location = "台灣，新北市三重區",
+                date = "9/14/2016",
             };
             Console.WriteLine("名稱:{0}, 血量:{1}, 性別:{2}, 體重:{3}, 屬性:{4}, 身高:{5}," +
-                "技能1:{6}, 技能2:{7}"
+                              "星星沙子:{6}, 妙蛙種子的糖果:{7}, 強化:{8}, 進化:{9}, 技能1:{10}, " +
+                              "技能1攻擊力:{11}, 技能2:{12}, 技能2攻擊力:{13}, 捕捉地點:{14}, 捕捉日期:{15}"
                 , pokemon.name
                 , pokemon.hp
                 , pokemon.gender
                 , pokemon.weight
                 , pokemon.attribute
                 , pokemon.height
+                , pokemon.star
+                , pokemon.candy
+                , pokemon.powerup
+                , pokemon.evolve
                 , pokemon.skill1
+                , pokemon.skill1dmg
                 , pokemon.skill2
+                , pokemon.skill2dmg
+                , pokemon.location
+                , pokemon.date
                 );
         }
     }
